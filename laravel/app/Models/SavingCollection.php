1e7fc72590806604c65770c5d18ae96241db8dc7
Daiveskhyle <73757950+Daiveskhyle@users.noreply.github.com>
2026-09-23 08:34:22 +0100

    Match legacy transaction timestamp schema

--- laravel/app/Models/SavingCollection.php
+++ laravel/app/Models/SavingCollection.php
@@ -9,6 +9,7 @@ class SavingCollection extends Model
     protected $table = 'saving_collections';
     protected $primaryKey = 'transaction_id';
     public $incrementing = false;
+    public $timestamps = false;
     protected $keyType = 'string';
     protected $guarded = [];
     protected $casts = ['amount' => 'decimal:2', 'balance_after' => 'decimal:2'];
