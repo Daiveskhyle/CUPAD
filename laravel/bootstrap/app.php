881994a94dfdbdbff272df3c62dec2b05e0f03ff
Daiveskhyle <73757950+Daiveskhyle@users.noreply.github.com>
2026-09-23 08:22:43 +0100

    Laravel migration: wire admin dashboard, roles and client module

--- laravel/bootstrap/app.php
+++ laravel/bootstrap/app.php
@@ -1,8 +1,9 @@
 <?php
 
-use IlluminateFoundationApplication;
-use IlluminateFoundationConfigurationExceptions;
-use IlluminateFoundationConfigurationMiddleware;
+use App\Http\Middleware\RoleMiddleware;
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->alias(['role' => RoleMiddleware::class]);
         $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
