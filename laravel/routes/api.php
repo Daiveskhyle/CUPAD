fe70d5c5a3779a2133002548f724373256f56606
Daiveskhyle <73757950+Daiveskhyle@users.noreply.github.com>
2026-09-23 08:25:51 +0100

    Add Laravel savings collection API

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\ClientController;
+use App\Http\Controllers\Api\SavingsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/health', fn () => response()->json(['success' => true, 'app' => 'CUPAD Laravel']));
@@ -12,4 +13,5 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
     Route::apiResource('clients', ClientController::class)->only(['index', 'show']);
     Route::get('/clients/{client}/portfolio', [ClientController::class, 'portfolio']);
+    Route::post('/savings/collect', [SavingsController::class, 'collect']);
 });
