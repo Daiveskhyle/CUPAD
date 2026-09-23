7cbf6b4eba48dbfdb028e70a88365862797980f3
Daiveskhyle <73757950+Daiveskhyle@users.noreply.github.com>
2026-09-23 08:34:10 +0100

    Fix legacy CUPAD model primary keys

--- laravel/app/Models/Saving.php
+++ laravel/app/Models/Saving.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Saving extends Model
 {
     protected $table = 'savings';
+    protected $primaryKey = 'id';
+    public $incrementing = false;
+    protected $keyType = 'string';
     protected $guarded = [];
     protected $casts = ['balance' => 'decimal:2'];
 }
