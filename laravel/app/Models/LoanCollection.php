c6e3c1333bbd5262e70233c13325c256faafc778
Daiveskhyle <73757950+Daiveskhyle@users.noreply.github.com>
2026-09-23 08:34:15 +0100

    Fix legacy CUPAD model primary keys

--- laravel/app/Models/LoanCollection.php
+++ laravel/app/Models/LoanCollection.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class LoanCollection extends Model
 {
     protected $table = 'loan_collections';
+    protected $primaryKey = 'transaction_id';
+    public $incrementing = false;
+    protected $keyType = 'string';
     protected $guarded = [];
     protected $casts = ['amount_collected' => 'decimal:2', 'remaining_balance' => 'decimal:2'];
 }
