bd00c16b52a269fd695291f6340798ce7a38a66d
Daiveskhyle <73757950+Daiveskhyle@users.noreply.github.com>
2026-09-23 08:38:19 +0100

    Support CUPAD string disbursement IDs

--- laravel/app/Models/Disbursement.php
+++ laravel/app/Models/Disbursement.php
@@ -7,12 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Disbursement extends Model
 {
     protected $table = 'disbursements';
+    protected $primaryKey = 'id';
+    public $incrementing = false;
+    protected $keyType = 'string';
     protected $guarded = [];
     protected $casts = [
         'principal' => 'decimal:2',
+        'interest_rate' => 'decimal:2',
         'total_payable' => 'decimal:2',
         'remaining_balance' => 'decimal:2',
         'date' => 'date',
+        'due_date' => 'date',
         'payoff_date' => 'date',
     ];
 }
